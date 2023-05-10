c446647ee399ff446edbd66b3e2a1e53c3db70bb
Jonah Broyer <jkbsk8er@yahoo.com>
2023-05-10 15:24:37 -0700

    Sequelize User

---
+++ models/user.js
@@ -0,0 +1,21 @@
+const { DataTypes } = require('sequelize')
+
+const sequelize = require('../lib/sequelize')
+
+const User = sequelize.define('user', {
+  userId: { type: DataTypes.INTEGER, allowNull: false },
+  name: { type: DataTypes.STRING, allowNull: false },
+  email: { type: DataTypes.STRING, allowNull: false },
+  password: { type: DataTypes.STRING, allowNull: false },
+  admin: { type: DataTypes.BOOLEAN, allowNull: false, defaultValue: false }
+})
+
+exports.User = User
+exports.UserClientFields = [
+  'userId',
+  'name',
+  'email',
+  'password',
+  'admin',
+  'businessId'
+]
