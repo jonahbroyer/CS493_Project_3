abf023a0e30ac14c7d16ddacc1b7393338f06ad3
Jonah Broyer <jkbsk8er@yahoo.com>
2023-05-12 16:47:41 -0700

    Hash and salt users password with Sequelize setter

--- models/user.js
+++ models/user.js
@@ -8,9 +8,16 @@ const User = sequelize.define('user', {
   userId: { type: DataTypes.INTEGER, allowNull: false },
   name: { type: DataTypes.STRING, allowNull: false },
   email: { type: DataTypes.STRING, allowNull: false },
-  password: { type: DataTypes.STRING, allowNull: false },
-  admin: { type: DataTypes.BOOLEAN, allowNull: false, defaultValue: false }
-})
+  admin: { type: DataTypes.BOOLEAN, allowNull: false, defaultValue: false },
+  password: { 
+    type: DataTypes.STRING,
+    allowNull: false,
+    async set(value) {
+      const passwordHash = await bcrypt.hash(User.password, 8);
+      this.setDataValue('password', hash(value));
+    }
+  }
+});
 
 exports.User = User
 exports.UserClientFields = [
